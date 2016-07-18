b9d877ba28d7df3da1a5fa8a9f9285eb86cd3944
Jonathan Gilbert <JonathanG@iQmetrix.com>
2016-07-18 09:28:15 -0500

    Initial project check-in.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace TestScrollBarHideThumb
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void chkSameMinMax_Checked(object sender, RoutedEventArgs e)
+		{
+			vscrlScroll.Maximum = vscrlScroll.Minimum;
+			hscrlScroll.Maximum = hscrlScroll.Minimum;
+		}
+
+		private void chkSameMinMax_Unchecked(object sender, RoutedEventArgs e)
+		{
+			vscrlScroll.Maximum = vscrlScroll.Minimum + 10;
+			hscrlScroll.Maximum = hscrlScroll.Minimum + 10;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+
+namespace TestScrollBarHideThumb
+{
+	class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			new Application().Run(new MainWindow());
+		}
+	}
+}
